13db2c0f0acc77508480d1a21bacffd77147c2c3
Asela Gunawardena <asela.gunawardena@techclick.com.au>
2025-04-22 12:32:23 +1000

    Initial commit

---
+++ myExpressApp/routes/index.js
@@ -0,0 +1,10 @@
+// filepath: myExpressApp/routes/index.js
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Star Wars' });
+});
+
+module.exports = router;
