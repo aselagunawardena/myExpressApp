160e866c5913f9756f2e2c3a1019235d2214f496
Asela Gunawardena <asela.gunawardena@techclick.com.au>
2025-04-22 14:11:29 +1000

    added some content

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Star Wars' });
 });
 
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Star Wars' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Star Wars' });
+});
+
 module.exports = router;
